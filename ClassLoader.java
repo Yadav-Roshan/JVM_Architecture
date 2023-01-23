5fba8661512b80f5012586e72b5eadbc86ef3b59
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-01-23 22:17:44 +0530

    Add files via upload

---
+++ ClassLoader.java
@@ -0,0 +1,7 @@
+public class ClassLoader{
+    public static void main(String[] args){
+        System.out.println(ClassLoader.class.getClassLoader());
+        System.out.println(String.class.getClassLoader());
+    }
+}
+
