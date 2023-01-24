c10ab4dffb4b033aeeb5f1e0c0cc603927f08670
Roshan Yadav <31336534+Yadav-Roshan@users.noreply.github.com>
2023-01-24 22:18:46 +0530

    Update heapDemo.java

--- heapDemo.java
+++ heapDemo.java
@@ -2,10 +2,11 @@ public class heapDemo
 {
     public static void main(String[] args)
     {
+        double mb = 1024*1024;
         Runtime r = Runtime.getRuntime();
-        System.out.println("Max Memory: " + r.maxMemory()/1024/1024);
-        System.out.println("Total Memory: " + r.totalMemory()/1024/1024);
-        System.out.println("Free Memory: " + r.freeMemory()/1024/1024);
-        System.out.println("Consumed Memory: " + (r.totalMemory() - r.freeMemory())/1024/1024);
+        System.out.println("Max Memory: " + r.maxMemory()/mb);
+        System.out.println("Total Memory: " + r.totalMemory()/mb);
+        System.out.println("Free Memory: " + r.freeMemory()/mb);
+        System.out.println("Consumed Memory: " + (r.totalMemory() - r.freeMemory())/mb);
     }
 }
